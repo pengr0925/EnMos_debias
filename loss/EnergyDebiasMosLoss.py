faaf9fbb30769288ba91ffd2bcd9924c38f4699f
pengr0925 <pengryuto@icloud.com>
2024-08-22 15:52:07 +0800

    ImageNet_LT && iNaturalist18

--- loss/EnergyDebiasMosLoss.py
+++ loss/EnergyDebiasMosLoss.py
@@ -26,9 +26,9 @@ class Energy_based_Debias_Loss(nn.Module):
 
     def forward(self, logits, targets, mixing_weights, epsilon=1e-10):
 
-        # logits = logits + torch.log(torch.tensor(np.array(self.class_bias) + 1e-12,
-        #                                          dtype=torch.float32,
-        #                                          device='cuda')).unsqueeze(0).unsqueeze(2).detach()
+        logits = logits + torch.log(torch.tensor(np.array(self.class_bias) + 1e-12,
+                                                 dtype=torch.float32,
+                                                 device='cuda')).unsqueeze(0).unsqueeze(2).detach()
 
         probs = self.calculate_probs(logits, mixing_weights)
         if (probs>1.0).sum() > 0 or (probs<=0.0).sum() > 0:
